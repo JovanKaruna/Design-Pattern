5d7be8045e7a209e61364a99a96ee4411f8c0bc7
jovankaruna <jovancahyadi@gmail.com>
2021-01-28 11:33:41 +0700

    Prototype with 2 bean

--- src/main/java/com/blibli/pattern/prototype/PrototypeApplication.java
+++ src/main/java/com/blibli/pattern/prototype/PrototypeApplication.java
@@ -35,15 +35,28 @@ public class PrototypeApplication {
                     .build();
         }
 
+        @Bean
+        @Scope("prototype")
+        public Customer premiumCustomer(){
+            return Customer.builder()
+                    .category("PREMIUM")
+                    .discount(50)
+                    .build();
+        }
+
     }
 
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(Configuration.class);
 
-        Customer customerStandard1 = context.getBean((Customer.class));
-        Customer customerStandard2 = context.getBean((Customer.class));
-        Customer customerStandard3 = context.getBean((Customer.class));
+        Customer customerStandard1 = context.getBean("standardCustomer", Customer.class);
+        Customer customerStandard2 = context.getBean("standardCustomer", Customer.class);
+        Customer customerStandard3 = context.getBean("standardCustomer", Customer.class);
+
+        Customer premiumCustomer1 = context.getBean("premiumCustomer",Customer.class);
+        Customer premiumCustomer2 = context.getBean("premiumCustomer",Customer.class);
+        Customer premiumCustomer3 = context.getBean("premiumCustomer",Customer.class);
 
         System.out.println(customerStandard1 == customerStandard2);
         System.out.println(customerStandard2 == customerStandard3);
@@ -51,6 +64,9 @@ public class PrototypeApplication {
         System.out.println(customerStandard1);
         System.out.println(customerStandard2);
         System.out.println(customerStandard3);
+        System.out.println(premiumCustomer1);
+        System.out.println(premiumCustomer2);
+        System.out.println(premiumCustomer3);
 
 //        Customer customerStandard1 = Customer.builder().id("1").name("Jovan")
 //                .category("STANDARD").discount(10).build();
